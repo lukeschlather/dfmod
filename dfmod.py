3556ab056828d8ef91f29579a05d8bdd66632997
grelphy <grelphy@gmail.com>
2010-08-31 09:42:59 -0400

    Made dry-run and show work with zip modules. Refactored for clarity and simplicity.

--- dfmod.py
+++ dfmod.py
@@ -34,66 +34,50 @@ Usage: dfmod.py install <target-release> <module>
 todo: implement zip
 '''
 
-if len(argv) < 2:
+# Determine installation target
+if len(argv) <= 2:
     print usage
     exit(1)
-elif argv[1] == 'install':
-    if len(argv) == 3:
-        targ = os.path.abspath('') # assume current working directory
-        mod = os.path.abspath(argv[2])
-    elif len(argv) == 4:
-        targ = os.path.abspath(argv[2])
-        mod = os.path.abspath(argv[3])
-    else:
-        print len(argv)
-        print 'Error: wrong number of arguments.'
-        print usage
-        exit(1)
+elif len(argv) == 3:
+	targ = os.path.abspath('')	# assume current working directory
+elif len(argv) == 4:
+	targ = os.path.abspath(argv[2])
+else
+	print usage
+	exit(1)
 
-    if not zipfile.is_zipfile(mod):
-        module = dfmodule(mod)
-		module.install(targ)
-    else:
-        tmpdir = tempfile.mkdtemp()
-        zipmod = zipfile.ZipFile(mod, 'r')
-        for member in zipmod.infolist():
-            path = os.path.normpath(os.path.join(tmpdir, member.filename))
-            if re.match(tmpdir + ".*", path):
-                zipmod.extract(member, tmpdir)
-            else:
-                print "Warning: skipping out-of-path file " + path + " (possibly malicious module?)"
-				if not promptYesNo("Continue with installation?", 0):
-                    shutil.rmtree(tmpdir)
-                    exit(1)
-        module = dfmodule(tmpdir)
-        module.install(targ)
-        shutil.rmtree(tmpdir)
-		
-    # todo: maybe handle this in a less stupid manner (but maybe not, if this works)
-
-elif argv[1] == 'dry-run':
-    if len(argv) == 3:
-        targ = os.path.abspath('') # assume current working directory
-        mod = os.path.abspath(argv[2])
-    elif len(argv) == 4:
-        targ = os.path.abspath(argv[2])
-        mod = os.path.abspath(argv[3])
-    else:
-        print len(argv)
-        print 'Error: wrong number of arguments.'
-        print usage
-        exit(1)
-        
+# Prepare installation source
+if not zipfile.is_zipfile(mod):
+	is_zipmod = false
     module = dfmodule(mod)
-    module.dryrun(targ)
-
-elif argv[1]=='show' and len(argv) == 3:
-    module=dfmodule(os.path.abspath(argv[2]))
-    module.pretty_print()
 else:
-    print usage
+	is_zipmod = true
+    tmpdir = tempfile.mkdtemp()
+    zipmod = zipfile.ZipFile(mod, 'r')
+    for member in zipmod.infolist():
+        path = os.path.normpath(os.path.join(tmpdir, member.filename))
+        if re.match(tmpdir + ".*", path):
+            zipmod.extract(member, tmpdir)
+        else:
+            print "Warning: skipping out-of-path file " + path + " (possibly malicious module?)"
+            if not promptYesNo("Continue with installation?", 0):
+                shutil.rmtree(tmpdir)
+                exit(1)
+    module = dfmodule(tmpdir)
 
+# Perform specified action
+if argv[1] == 'install':
+	module.install(targ)
+elif argv[1] == 'dry-run':
+	module.dryrun(targ)
+elif argv[1] == 'show':
+	module.pretty_print()
+else
+	print usage
+	exit(1)
 
+# Cleanup temporary directories
+if is_zipmod:
+	shutil.rmtree(tmpdir)
 
 exit(0)
-
